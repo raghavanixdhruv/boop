7cdd220a0363d026511c26671f4e74c637ee9ac3
raghavanixdhruv <103164394+raghavanixdhruv@users.noreply.github.com>
2022-04-07 10:10:38 +0530

    Create PR1.cpp

---
+++ PR1.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int x,y;
+clrscr();
+cout<<"Enter x:";
+cin>>x;
+cout<<"Enter y:";
+cin>>y;
+cout<<"Addition is:"<<(x+y);
+getch();
+return 0;
+}
